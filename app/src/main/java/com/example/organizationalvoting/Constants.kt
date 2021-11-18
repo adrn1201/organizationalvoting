4cd57d88f3b393d70594aba5e60d70719ecd1f67
Jan Vincent Carlo Dizon <11903622@benilde.edu.ph>
2021-11-18 18:29:01 +0800

    Constants update

--- app/src/main/java/com/example/organizationalvoting/Constants.kt
+++ app/src/main/java/com/example/organizationalvoting/Constants.kt
@@ -2,8 +2,8 @@ package com.example.organizationalvoting
 
 const val REQUEST_CODE = 1
 const val RETURN_KEY = "return key"
-const val USERNAME = "name"
-const val NAMEUSER = "name2"
+const val FNAME = "name"
+const val LNAME = "name2"
 const val INPUT_PRESIDENT ="pres"
 const val INPUT_VP ="vp"
 const val INPUT_SECRETARY ="secr"
