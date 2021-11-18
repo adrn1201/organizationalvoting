1d9263b8643db946309f06d79292f2ace6f6f92a
Juan Carlo Nepomuceno <nepomuceno.juancarlo@benilde.edu.ph>
2021-11-18 20:02:28 +0800

    Added function on ConfirmationActivity

--- app/src/main/java/com/example/organizationalvoting/ConfirmationActivity.kt
+++ app/src/main/java/com/example/organizationalvoting/ConfirmationActivity.kt
@@ -11,6 +11,8 @@ class ConfirmationActivity : AppCompatActivity() {
 
     private val message by lazy {binding.txtMessage}
     private val date by lazy {binding.txtDate}
+    private val conn by lazy {binding.txtCon}
+    private var confirmation = "Confirmation"
     private  var end = "Thank you!"
     //private  var time = "${Calendar.getInstance().time}"
 
@@ -20,16 +22,26 @@ class ConfirmationActivity : AppCompatActivity() {
         binding = ActivityConfirmationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        fun generateRandomString(len: Int = 15): String{
+            val alphanumerics = CharArray(26) { it -> (it + 97).toChar() }.toSet()
+                .union(CharArray(9) { it -> (it + 48).toChar() }.toSet())
+            return (0..len-1).map {
+                alphanumerics.toList().random()
+            }.joinToString("")
+        }
+
 
         val enteredfName = intent.getStringExtra(FNAME)
         val enteredlName = intent.getStringExtra(LNAME)
 
+        confirmation = "Confirmation: " + "#" + generateRandomString()
         end = "Thank you! $enteredfName $enteredlName"
 
         val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
 
         binding.txtDate.text = formatter.format(Date())
 
+        conn.text = confirmation
         message.text = end
         //date.text = time
 
